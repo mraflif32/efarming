4d0199787d61ddb2e4d19a3ffb0c29f643ac25c9
Muhammad Fadillah <mraflif32@yahoo.com>
2021-07-14 22:37:43 +0700

    Add trigger

--- server/index.js
+++ server/index.js
@@ -43,15 +43,39 @@ const config = [
   },
 ];
 
+const trigs = [
+  {
+    sensor: 'sensor1',
+    servo: 'pump1',
+    type: 'lesser',
+    value: 1,
+    intv: 5000,
+    duration: 10000,
+  },
+];
+
+// INITIATING VARS
+
 var sensArray = [];
 var servArray = [];
+
 var sensInitArray = [];
 var servInitArray = [];
+
+var trigArray = [];
+var servTimeoutArray = [];
+
 var messageSensor = null;
 var messageServo = null;
 
 var valArray = {};
 var valArrayServ = {};
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+
+// READ CONFIG, INIT SENSOR SERVO TRIGGER
 
 for (let i = 0; i < config.length; i += 1) {
   console.log('comp', config[i]);
@@ -76,13 +100,30 @@ for (let i = 0; i < config.length; i += 1) {
   };
 }
 
+for (let i = 0; i < trigs.length; i += 1) {
+  trigArray.push(setInterval(() => {
+    //let sensorIndex = sensArray.findIndex((item) => item.name === trigs[i].sensor);
+    let servoIndex = servArray.findIndex((item) => item.name === trigs[i].servo);
+    console.log('trigger', servoIndex);
+    if (valArray[trigs[i].sensor] && valArrayServ[trigs[i].servo] && !servTimeoutArray[trigs[i].servo]) {
+      if (trigs[i].type === 'bigger' && valArray[trigs[i].sensor] > trigs[i].value) {
+        servInitArray[servoIndex].writeSync(0);
+        servTimeoutArray[trigs[i].servo] = setTimeout(() => {
+          servInitArray[servoIndex].writeSync(1);
+          servTimeoutArray[trigs[i].servo] = null;
+        }, trigs[i].duration);
+      } else if (trigs[i].type === 'lesser' && valArray[trigs[i].sensor] < trigs[i].value) {
+        servInitArray[servoIndex].writeSync(0);
+        servTimeoutArray[trigs[i].servo] = setTimeout(() => {
+          servInitArray[servoIndex].writeSync(1);
+          servTimeoutArray[trigs[i].servo] = null;
+        }, trigs[i].duration);
+      }
+    }
+  }, trigs[i].intv));
+}
+
 console.log('length ', sensArray.length);
-//var valArray = {
-  //sensor1: null,
-  //sensor2: null,
-//};
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
 
 function sendMessage() {
   eventEmitter.emit('send-message', valArray);
@@ -116,9 +157,9 @@ var readPoll = setInterval(() => {
   //sendMessage();
 }, 3000);
 
-setTimeout(() => {
-  servInitArray[0].writeSync(0);
-}, 10000);
+//setTimeout(() => {
+  //servInitArray[0].writeSync(0);
+//}, 10000);
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -195,6 +236,13 @@ wss.on('close', function close() {
 process.on('SIGINT', _ => {
   clearInterval(readPoll);
   clearInterval(interval);
+  
+  for (let i = 0; i < trigArray.length; i++) {
+    clearInterval(trigArray[i]);
+  };
+  for (const timeout in servTimeoutArray) {
+    if (timeout) clearTimeout(timeout);
+  };
   for (let i = 0; i < sensInitArray.length; i++) {
     sensInitArray[i].unexport();
   };
